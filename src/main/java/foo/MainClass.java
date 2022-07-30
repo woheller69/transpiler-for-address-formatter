73cbacb1fc1c7efb3ecb3820210c3e283319358d
woheller69 <wolfgang.heller@web.de>
2022-07-30 07:30:46 +0200

    First commit

---
+++ src/main/java/foo/MainClass.java
@@ -0,0 +1,8 @@
+package foo;
+
+public class MainClass {
+    public static void main(String[] arg) {
+        String hi = "Hello World, run ./gradlew jar to create jar file in build/libs ";
+        System.out.println(hi);
+    }
+}
